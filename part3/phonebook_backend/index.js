638374cf27711e28caae65eb1b83d933a84a18b6
lazicmarko <markolazic1995@gmail.com>
2021-04-30 17:52:49 +0200

    fixed app.put on backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -101,7 +101,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { runValidators: true, new: true })
+  Person.findByIdAndUpdate(request.params.id, person, { runValidators: true, new: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
